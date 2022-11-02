72a1560c3695f069237b993012b3ff56820b5e33
Pranes46 <92832458+Pranes46@users.noreply.github.com>
2022-11-01 20:06:05 -0400

    Update symmetric_tree.py

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution:  #tc - o(n(. sc - o(n)
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         
         
